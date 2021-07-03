30d374ca456933976a6a9e0d07f04129465c6172
Christian Urzagasti <christian.urzagasti@globant.com>
2021-07-03 14:36:12 -0300

    Adding gitignore file and update container backend

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name   = "tf-rg"
     storage_account_name  = "tfsta1000"
-    container_name        = "terraform"
+    container_name        = "tfstate"
     key                   = "FeL3Ec+Q6Y71kP59UzowImCTYppFoCYJosfcLpdpByIbMhEfoXhNgHxj2+79LfGu6P0SlzP0xfQ+vJkc4TbMyw=="
 }
 
