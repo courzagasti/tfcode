663ac2f675555388544a0882d2b135a7a8a7ba30
Christian Urzagasti <christian.urzagasti@globant.com>
2021-07-03 14:19:33 -0300

    Initial Code

---
+++ backend.sh
@@ -0,0 +1,6 @@
+backend "azurerm" {
+    resource_group_name   = "tf-rg"
+    storage_account_name  = "tfsta1000"
+    container_name        = "terraform"
+    key                   = "FeL3Ec+Q6Y71kP59UzowImCTYppFoCYJosfcLpdpByIbMhEfoXhNgHxj2+79LfGu6P0SlzP0xfQ+vJkc4TbMyw=="
+}
